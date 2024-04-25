d13fe13fbe28127d0a3c5b697b5284335b00be57
SriGit39 <168125032+SriGit39@users.noreply.github.com>
2024-04-26 01:26:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let ema = 'uSEr1500@gmail.com';
 window.VWO = window.VWO || [];
 // Replace TAG_VALUE with your actual tag value
 window.VWO.push(['tag', 'email', ema, 'user']);
+
+let gend = "male";
+window.VWO = window.VWO || [];
+// Replace TAG_VALUE with your actual tag value
+window.VWO.push(['tag', 'Gender', gend, 'user']);
